1367706c4ddc02733a055c3d740642e3fa085496
taylorcoders <69030890+taylorcoders@users.noreply.github.com>
2022-07-02 01:49:24 +0800

    Update config_update.py

--- config_update.py
+++ config_update.py
@@ -5,6 +5,6 @@ import shutil,os
 
  
 #重命名文件
-shutil.move("/root/ql/scripts/raw_main_config.sh","/root/ql/scripts/config.sh")
+shutil.move("\\root\\ql\\scripts\\raw_main_config.sh","\\root\\ql\\scripts\\config.sh")
 #移动文件
-shutil.move("/root/ql/scripts/config.sh","/root/ql/config/config.sh")
+shutil.move("\\root\\ql\\scripts\\config.sh","\\root\\ql\\config\\config.sh")
