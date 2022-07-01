8ea9be4c97692a48912537f6d9e42edac065b497
taylorcoders <69030890+taylorcoders@users.noreply.github.com>
2022-07-02 02:35:54 +0800

    Update config_update.py

--- config_update.py
+++ config_update.py
@@ -4,7 +4,3 @@ import sys
 
   print sys.argv[0]
 
-#print os.getcwd() #获取当前工作目录路径
-#print os.path.abspath('.') #获取当前文件目录路径
-#print os.path.abspath('raw_main_config.sh') #获取当前目录文件下的文件目录路径
-#shutil.move("/root/ql/scripts/raw_main_config.sh", "/root/ql/config/config.sh")
