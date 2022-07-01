b8f6b5bf865b8cead94db80189fa35019aabcb6e
taylorcoders <69030890+taylorcoders@users.noreply.github.com>
2022-07-02 02:07:15 +0800

    111

--- config_update.py
+++ config_update.py
@@ -3,8 +3,8 @@ import shutil,os
  
 
 #test
- 
+#os.rename('raw_main_config.sh', 'config.sh') # 重命名文件
 #重命名文件
-shutil.move("\\root\\ql\\scripts\\raw_main_config.sh","\\root\\ql\\scripts\\config.sh")
+#shutil.move("\\root\\ql\\scripts\\raw_main_config.sh","\\root\\ql\\scripts\\config.sh")
 #移动文件
-shutil.move("\\root\\ql\\scripts\\config.sh","\\root\\ql\\config\\config.sh")
+shutil.move(r'.\raw_main_config.sh', r'.\config/config.sh')
