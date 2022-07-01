4c42cb26352718d261fc089ef909842a4fc1e7df
taylorcoders <69030890+taylorcoders@users.noreply.github.com>
2022-07-02 01:47:21 +0800

    Add files via upload

---
+++ config_update.py
@@ -0,0 +1,10 @@
+ #导入shutil模块和os模块
+import shutil,os
+ 
+
+
+ 
+#重命名文件
+shutil.move("/root/ql/scripts/raw_main_config.sh","/root/ql/scripts/config.sh")
+#移动文件
+shutil.move("/root/ql/scripts/config.sh","/root/ql/config/config.sh")
