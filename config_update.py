807674d1ea3badc0a2e611288c39397ebdba01c8
taylorcoders <69030890+taylorcoders@users.noreply.github.com>
2022-07-02 02:33:25 +0800

    Update config_update.py

--- config_update.py
+++ config_update.py
@@ -1,5 +1,6 @@
-import shutil,os
-
+import shutil
+imoprt os
+print(1)
 print os.getcwd() #获取当前工作目录路径
 print os.path.abspath('.') #获取当前文件目录路径
 print os.path.abspath('raw_main_config.sh') #获取当前目录文件下的文件目录路径
