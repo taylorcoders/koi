ebbbb55f18b3db918d09a6f575bec917a3ef76db
taylorcoders <69030890+taylorcoders@users.noreply.github.com>
2022-07-02 02:46:44 +0800

    Update and rename con_update.py to c_update.py

---
+++ c_update.py
@@ -0,0 +1,8 @@
+"""
+111
+"""
+
+import shutil
+
+shutil.move("/root/ql/scripts/raw_main_config.sh", "/root/ql/config/config.sh")
+
--- con_update.py
+++
@@ -1,6 +0,0 @@
-import shutil
-import os
-import sys
-
-  print sys.argv[0]
-
